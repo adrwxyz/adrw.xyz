5def084a660c5fcfc7d902287bbb8a5cab91f5a7
Andrew Alexander <adrw@squareup.com>
2019-05-10 02:12:27 -0400

    More cms preview work

--- src/cms/preview-templates/MdxPreview.js
+++ src/cms/preview-templates/MdxPreview.js
@@ -1,8 +1,15 @@
 import React from "react"
 import { MDXRenderer } from "gatsby-mdx"
+import { ThemeProvider } from "emotion-theming"
+import { Layout } from "gatsby-theme-digital-garden"
+import theme from "../../gatsby-theme-digital-garden/theme"
 
 const MdxPreview = ({ widgetFor }) => (
-  <MDXRenderer children={widgetFor("body")} />
+  <ThemeProvider theme={theme}>
+    <Layout>
+      <MDXRenderer children={widgetFor("body")} />
+    </Layout>
+  </ThemeProvider>
 )
 
 export default MdxPreview
