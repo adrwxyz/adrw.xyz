3d6e01d4d17e8c865c5087771e29f6bf54b7a2ce
Andrew (Paradi) Alexander <gh@adrw.xyz>
2019-06-15 09:18:57 -0400

    Migrate to @westegg/gatsby-theme-digital-garden (#69)

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,88 +4,33 @@ module.exports = {
   },
   __experimentalThemes: [
     {
-      resolve: "gatsby-theme-digital-garden",
+      resolve: "@westegg/gatsby-theme-digital-garden",
       options: {
-        notesPath: "/notes"
-      }
-    },
-    {
-      resolve: "gatsby-theme-digital-garden-blog",
-      options: {
-        postsPath: "/blog"
-      }
-    },
-    {
-      resolve: "gatsby-theme-digital-garden-portfolio",
-      options: {
+        header: {
+          home: {
+            href: "/",
+            label: "/img/logos/hfm.png"
+          },
+          links: [
+            {
+              href: "/projects",
+              label: "Projects"
+            },
+            {
+              href: "/blog",
+              label: "Writing"
+            },
+            {
+              href: "/notes",
+              label: "Notes"
+            }
+          ]
+        },
+        notesPath: "/notes",
+        postsPath: "/blog",
         projectsPath: "/projects",
         projects: "projects"
       }
     }
-  ],
-  plugins: [
-    {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/static/img`,
-        name: "uploads"
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/img`,
-        name: "images"
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/static/assets`,
-        name: "assets"
-      }
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve: "gatsby-remark-relative-images",
-            options: {
-              name: "uploads"
-            }
-          },
-          {
-            resolve: "gatsby-remark-relative-images",
-            options: {
-              name: "assets"
-            }
-          },
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 2048
-            }
-          },
-          {
-            resolve: "gatsby-remark-copy-linked-files",
-            options: {
-              destinationDir: "static"
-            }
-          }
-        ]
-      }
-    },
-    "gatsby-plugin-offline",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-redirects",
-    "gatsby-plugin-netlify-cms",
-    "gatsby-plugin-netlify" // make sure to keep it last in the array
   ]
 }
