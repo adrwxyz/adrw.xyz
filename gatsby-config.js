0d15a2b61646821a93cce91ae8165c8feebdbf27
Andrew Alexander <adrw@squareup.com>
2020-04-25 17:12:40 -0400

    Use plugin for favicon instead

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,37 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./static/favicon.png",
+
+        // WebApp Manifest Configuration
+        appName: "adrw.xyz", // Inferred with your package.json
+        appDescription: "Andrew Alexander | ADRW Digital",
+        developerName: "Andrew Alexander",
+        developerURL: "adrw.xyz",
+        dir: "auto",
+        lang: "en-US",
+        background: "#fff",
+        theme_color: "#fff",
+        display: "standalone",
+        orientation: "any",
+        start_url: "/?homescreen=1",
+        version: "1.0",
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
+    {
       resolve: "gatsby-plugin-matomo",
       options: {
         siteId: "1",
