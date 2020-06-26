c07e012d28c12600b311dd04de2d1e2dcdde8648
Andrew Alexander <adrw@squareup.com>
2020-06-26 11:40:26 -0400

    Fix gatsby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,46 +50,6 @@ module.exports = {
       }
     },
     {
-      resolve: "@westegg/gatsby-theme-core",
-      options: {
-        matomoConfig: {
-          siteId: "1",
-          matomoUrl: "https://matomo.adrw.xyz",
-          siteUrl: "https://adrw.xyz"
-        }
-      }
-    },
-    // {
-    //   resolve: "@westegg/gatsby-theme-digital-garden",
-    //   options: {
-    //     header: {
-    //       home: {
-    //         href: "/",
-    //         label: "/img/logos/h.png"
-    //       },
-    //       links: [
-    // {
-    //   href: "/projects",
-    //   label: "Projects"
-    // },
-    // {
-    //   href: "/blog",
-    //   label: "Writing"
-    // },
-    // {
-    //   href: "/notes",
-    //   label: "Notes"
-    // }
-    //       ]
-    //     },
-    //     notesPath: "/notes",
-    //     postsPath: "/blog",
-    //     projectsPath: "/projects",
-    //     projects: "projects"
-    //   }
-    // },
-
-    {
       resolve: "gatsby-plugin-matomo",
       options: {
         siteId: "1",
@@ -121,6 +81,8 @@ module.exports = {
           }
         ]
       }
-    }
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`
   ]
 }
