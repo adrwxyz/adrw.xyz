e13eb51252a7104fc1fa60e81ce37d760c5eb09a
Andrew Alexander <adrw@squareup.com>
2022-06-15 11:26:52 -0400

    Lint

--- src/@mkitio/gatsby-theme-password-protect/components/PasswordProtect.js
+++ src/@mkitio/gatsby-theme-password-protect/components/PasswordProtect.js
@@ -1,64 +1,64 @@
 /**
  * Write-only the password as cookie
  */
-import { setSessionPassword } from '@mkitio/gatsby-theme-password-protect/src/utils/utils';
-import React, { useState } from 'react';
+import { setSessionPassword } from "@mkitio/gatsby-theme-password-protect/src/utils/utils"
+import React, { useState } from "react"
 
 const styles = {
   wrapper: {
-    height: '100vh',
-    background: '#777',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
+    height: "100vh",
+    background: "#777",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
   },
   input: {
-    width: '100%',
-    height: '48px',
-    borderRadius: '4px'
+    width: "100%",
+    height: "48px",
+    borderRadius: "4px"
   },
   button: {
-    width: '100%',
-    height: '48px',
-    background: '#111',
-    color: '#fff',
-    border: '2px solid white',
-    borderRadius: '4px',
-    marginTop: '16px',
-    textTransform: 'uppercase',
-    fontWeight: '300',
-    fontFamily: 'sans-serif'
+    width: "100%",
+    height: "48px",
+    background: "#111",
+    color: "#fff",
+    border: "2px solid white",
+    borderRadius: "4px",
+    marginTop: "16px",
+    textTransform: "uppercase",
+    fontWeight: "300",
+    fontFamily: "sans-serif"
   },
   buttonHover: {
-    background: '#fff',
-    color: '#000000'
+    background: "#fff",
+    color: "#000000"
   },
   link: {
-    borderBottom: '1px solid #fff',
-    color: '#fff',
-    fontFamily: 'sans-serif',
-    textDecoration: 'none'
-  },
-};
+    borderBottom: "1px solid #fff",
+    color: "#fff",
+    fontFamily: "sans-serif",
+    textDecoration: "none"
+  }
+}
 
 const PasswordProtect = () => {
-  const [password, setPassword] = useState('');
-  const [isButtonHovered, buttonHover] = useState(false);
+  const [password, setPassword] = useState("")
+  const [isButtonHovered, buttonHover] = useState(false)
 
   const onSubmit = event => {
-    event.preventDefault();
-    setSessionPassword(password);
-    window.location.reload(); // eslint-disable-line
-  };
+    event.preventDefault()
+    setSessionPassword(password)
+    window.location.reload() // eslint-disable-line
+  }
 
   return (
     <div style={styles.wrapper}>
       <img src="../../img/logos/h-300.png" />
 
-      <h1 style={{ color: '#fff' }}>🔐</h1>
+      <h1 style={{ color: "#fff" }}>🔐</h1>
 
-      <form onSubmit={onSubmit} style={{ width: '320px' }}>
+      <form onSubmit={onSubmit} style={{ width: "320px" }}>
         <input
           name="password"
           type="password"
@@ -94,15 +94,11 @@ const PasswordProtect = () => {
         Go Back
       </a>
       <br />
-      <a
-        href="/"
-        rel="noopener noreferrer"
-        style={styles.link}
-      >
+      <a href="/" rel="noopener noreferrer" style={styles.link}>
         Home
       </a>
     </div>
-  );
-};
+  )
+}
 
-export default PasswordProtect;
+export default PasswordProtect
